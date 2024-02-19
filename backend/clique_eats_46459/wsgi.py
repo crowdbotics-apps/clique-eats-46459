33ec28d337fc28c36f307d322f8098c0f999816c
Crowdbotics <team@crowdbotics.com>
2024-02-19 04:31:22 +0000

    Initial commit

---
+++ backend/clique_eats_46459/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for clique_eats_46459 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "clique_eats_46459.settings"
+)
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "clique-eats-46459",
+  url: "https://clique-eats-46459.botics.co",
+  api: "https://clique-eats-46459.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
